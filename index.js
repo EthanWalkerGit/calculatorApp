878eddacf8b16f68b5cb55a8f81d9a853d2426e8
Ethan Walker <ethanwalker576@gmail.com>
2024-03-02 20:20:14 -0500

    added colours

--- index.js
+++ index.js
@@ -19,3 +19,71 @@ function calculate(){
         display.value = 'Error';
     }
 }
+
+function changeColourRed(){
+    var element = document.querySelector('#calculator');
+    var display = document.getElementById('display');
+    var button = document.querySelectorAll('button');
+    var operatorButton = document.querySelectorAll('.operator-btn');
+
+    element.style.backgroundColor = "hsl(0, 100%, 55%)";
+    display.style.backgroundColor = "hsl(0, 100%, 55%)";
+
+    button.forEach(function(button) {
+        button.style.backgroundColor = "hsl(0, 100%, 60%)";
+        button.style.borderColor = "hsl(0, 100%, 70%)";
+        button.style.color = "hsl(0, 0%, 100%)";
+    });
+    
+    operatorButton.forEach(function(button) {
+        button.style.backgroundColor = "hsl(0, 100%, 70%)";
+        button.style.borderColor = "hsl(0, 100%, 80%)";
+    });
+
+}
+
+function changeColourGreen(){
+    var element = document.querySelector('#calculator');
+    var display = document.getElementById('display');
+    var button = document.querySelectorAll('button');
+    var operatorButton = document.querySelectorAll('.operator-btn');
+
+    element.style.backgroundColor = "hsl(113, 100%, 35%)";
+    display.style.backgroundColor = "hsl(113, 100%, 35%)";
+    display.style.color = "hsl(0, 0%, 20%)";
+
+    button.forEach(function(button) {
+        button.style.backgroundColor = "hsl(113, 100%, 50%)";
+        button.style.borderColor = "hsl(113, 100%, 70%)";
+        button.style.color = "hsl(0, 0%, 20%)";
+    });
+    
+    operatorButton.forEach(function(button) {
+        button.style.backgroundColor = "hsl(113, 100%, 70%)";
+        button.style.borderColor = "hsl(113, 100%, 80%)";
+    });
+
+}
+
+function changeColourBlue(){
+    var element = document.querySelector('#calculator');
+    var display = document.getElementById('display');
+    var button = document.querySelectorAll('button');
+    var operatorButton = document.querySelectorAll('.operator-btn');
+
+    element.style.backgroundColor = "hsl(229, 100%, 35%)";
+    display.style.backgroundColor = "hsl(229, 100%, 35%)";
+    display.style.color = "hsl(0, 0%, 100%)";
+
+    button.forEach(function(button) {
+        button.style.backgroundColor = "hsl(229, 100%, 50%)";
+        button.style.borderColor = "hsl(229, 100%, 70%)";
+        button.style.color = "hsl(0, 0%, 100%)";
+    });
+    
+    operatorButton.forEach(function(button) {
+        button.style.backgroundColor = "hsl(229, 100%, 70%)";
+        button.style.borderColor = "hsl(229, 100%, 80%)";
+    });
+
+}
